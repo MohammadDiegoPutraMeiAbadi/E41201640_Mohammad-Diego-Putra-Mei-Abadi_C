ecda27c1f13822053a75007d0e9645aa3ead497e
MohammadDiegoPutraMeiAbadi <diegoputra120602@gmail.com>
2021-04-04 20:08:26 +0700

    Update TugasB.java

--- Minggu4/TugasB.java
+++ Minggu4/TugasB.java
@@ -11,15 +11,34 @@ package tugas.wsid.w04;
  */
 public class TugasB {
     public static void main(String[] args) {
-        System.out.println("        Do_while");
+        
+        int i = 2;
+        
+        System.out.println("        Do_while"            );
         System.out.println("Bilangan kelipatan 2 (1-100)");
         System.out.println("============================");
-        
-        int i = 1;
         do {
-            System.out.print(i + " ");
-            i *= 2;
+            System.out.print(" "+i+" ");
+        
+            i+=2;
+        } while (i<=100);
+        
+     System.out.println("");
+     System.out.println("");   
+     System.out.println("");
+     
+        int j = 1;
+        
+         System.out.println("        Do_while           ");
+        System.out.println("Perkalian 2 (1-100)         ");
+        System.out.println("============================");
+        do {
+            System.out.print(" "+j+" ");
+            
+            j*= 2;
         } 
-        while ( i < 100);
+        while ( j < 100);
+        
+        System.out.println("");
     }
 }
